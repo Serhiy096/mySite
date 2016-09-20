a5ea5a6b3be9835e468925333d745906d8e361ad
Serhiy <seruy097@gmail.com>
2016-09-20 17:09:41 +0300

    I'm here

--- GravityDemo/ViewController.swift
+++ GravityDemo/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
+    var firstContact = false
+    var square: UIView!
+    var snap: UISnapBehavior!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        square = UIView(frame: CGRect(x:100, y:100, width:100, height:100))
+        square.backgroundColor = UIColor.grayColor()
+        view.addSubview(square)
+        
+        let barrier = UIView(frame: CGRect(x:0, y:300, width:130, height:20))
+        barrier.backgroundColor = UIColor.redColor()
+        view.addSubview(barrier)
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [square])
+        animator.addBehavior(gravity)
+       
+        collision = UICollisionBehavior(items: [square])
+        collision.addBoundaryWithIdentifier("barrier", forPath: UIBezierPath(rect: barrier.frame))
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        
+        collision.collisionDelegate = self
+        
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?)
+        {
+            
+        if (snap != nil) {
+            animator.removeBehavior(snap)
+        }
+        
+        let touch = touches.first
+        snap = UISnapBehavior(item: square, snapToPoint: touch!.locationInView(view))
+        animator.addBehavior(snap)
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
